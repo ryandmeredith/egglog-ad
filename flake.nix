43812d083b416f9efbad51026aadf61384dacfd8
Ryan Meredith <ryan@ryanmeredith.com>
2025-11-26 02:30:16 -0500

    Latest egglog

--- flake.nix
+++ flake.nix
@@ -1,8 +1,16 @@
 {
+  inputs = {
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    egglog-src = {
+      url = "github:egraphs-good/egglog";
+      flake = false;
+    };
+  };
   outputs =
     inputs@{
       nixpkgs,
       flake-parts,
+      egglog-src,
       ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } (
@@ -11,10 +19,19 @@
         systems = lib.systems.flakeExposed;
         perSystem =
           { pkgs, ... }:
+          let
+            egglog1 = pkgs.rustPlatform.buildRustPackage {
+              name = "egglog";
+              version = "1.0.0";
+              src = egglog-src;
+              cargoLock.lockFile = "${egglog-src}/Cargo.lock";
+              doCheck = false;
+            };
+          in
           {
             devShells.default = pkgs.mkShell {
               packages = with pkgs; [
-                egglog
+                egglog1
                 rlwrap
                 graphviz
               ];
