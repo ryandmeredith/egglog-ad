c05e982b7de539b4890d4741fff9d1d7214f8ebd
Ryan Meredith <ryan@ryanmeredith.com>
2025-12-10 04:07:13 -0500

    Added complicated extra loop rule

--- src/optim.rs
+++ src/optim.rs
@@ -1,6 +1,8 @@
 use egglog::{
-    EGraph, Error, RunReport,
+    ArcSort, EGraph, Error, ExecutionState, Primitive, RunReport, Value,
     ast::{Command, Rewrite, RustSpan, Span},
+    constraint::{SimpleTypeConstraint, TypeConstraint},
+    sort::SetContainer,
     span, var,
 };
 
@@ -21,6 +23,12 @@ fn rewrite(lhs: impl DLike, rhs: impl DLike, subsume: bool) -> Command {
 }
 
 pub(crate) fn add_to_egraph(eg: &mut EGraph) -> Result<(), Error> {
+    eg.declare_sort("VarSet", &Some(("Set".into(), vec![var!("i64")])), span!())?;
+    let sort = eg
+        .get_sort_by_name("VarSet")
+        .expect("VarSet already defined")
+        .clone();
+    eg.add_primitive(SetShift { sort });
     eg.parse_and_run_program(Some("optim.egg".into()), include_str!("optim.egg"))?;
     let x = &D(var!("x"));
     let y = &D(var!("y"));
@@ -103,4 +111,43 @@ pub fn grad_opt(f: impl DLike) -> Result<D, Error> {
 
     let (dag, term, _) = eg.extract_value(&sort, val)?;
     Ok(D(dag.term_to_expr(&term, span!())))
+}
+
+#[derive(Clone)]
+struct SetShift {
+    sort: ArcSort,
+}
+
+impl Primitive for SetShift {
+    fn name(&self) -> &str {
+        "set-shift"
+    }
+
+    fn get_type_constraints(&self, span: &Span) -> Box<dyn TypeConstraint> {
+        SimpleTypeConstraint::new(
+            "set-shift",
+            vec![self.sort.clone(), self.sort.clone()],
+            span.clone(),
+        )
+        .into_box()
+    }
+
+    fn apply(&self, exec_state: &mut ExecutionState, args: &[Value]) -> Option<Value> {
+        let [arg] = args else { return None };
+        let cv = exec_state.container_values();
+        let bv = exec_state.base_values();
+        let new = SetContainer {
+            do_rebuild: true,
+            data: cv
+                .get_val::<SetContainer>(*arg)?
+                .data
+                .iter()
+                .filter_map(|x| {
+                    let x: i64 = bv.unwrap(*x);
+                    if x == 0 { None } else { Some(bv.get(x - 1)) }
+                })
+                .collect(),
+        };
+        Some(cv.register_val(new, exec_state))
+    }
 }
